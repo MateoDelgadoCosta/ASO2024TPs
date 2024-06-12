8055dbedeb50330fcf304374a44229664d2f6364
zCosmi <mateodelsta@gmail.com>
2024-06-12 16:17:49 -0300

    TP5 entregado

--- TP5/clima.sh
+++ TP5/clima.sh
@@ -1,17 +1,44 @@
-#! /bin/bash
-echo API de ejemplo: http://api.weatherapi.com/v1/current.json?key=74eaf0f05faa4bf69db190215241106&q=BuenosAires&aqi=yes
+#!/bin/bash
 
-read -p "Ingresa la URL de la API: " api
+echo "Elige una ciudad para consultar el clima:"
+echo "1. Londres"
+echo "2. Buenos Aires"
+echo "3. Tokio"
+echo "4. Madrid"
+
+read -p "Ingresa el numero correspondiente a la ciudad: " choice
+
+case $choice in
+  1)
+    city="London"
+    ;;
+  2)
+    city="Buenos Aires"
+    ;;
+  3)
+    city="Tokyo"
+    ;;
+  4)
+    city="Madrid"
+    ;;
+  *)
+    echo "Opcion no valida."
+    exit 1
+    ;;
+esac
+
+api="http://api.weatherapi.com/v1/current.json?key=74eaf0f05faa4bf69db190215241106&q=$city&lang=es"
+
 response=$(curl -s $api)
 
-city=$(echo $response | jq -r '.location.name')
-temperature=$(echo $response | jq -r '.current.temp_c')
-condition=$(echo $response | jq -r '.current.condition.text')
-humidity=$(echo $response | jq -r '.current.humidity')
-wind=$(echo $response | jq -r '.current.wind_kph')
+ciudad=$(echo $response | jq -r '.location.name')
+temperatura=$(echo $response | jq -r '.current.temp_c')
+condicion=$(echo $response | jq -r '.current.condition.text')
+humedad=$(echo $response | jq -r '.current.humidity')
+viento=$(echo $response | jq -r '.current.wind_kph')
 
-echo "Clima en $city:"
-echo "Temperatura: $temperature °C"
-echo "Condición: $condition"
-echo "Humedad: $humidity %"
-echo "Viento: $wind kph"
+echo "Clima en $ciudad:"
+echo "Temperatura: $temperatura °C"
+echo "Condición: $condicion"
+echo "Humedad: $humedad %"
+echo "Viento: $viento kph"
--- TP5/contpal.sh
+++ TP5/contpal.sh
@@ -1,3 +1,14 @@
-cat < hola.txt
+#!/bin/bash
 
-wc hola.txt 
+echo "Contenido del archivo texto.txt:"
+cat texto.txt
+echo
+
+lineas=$(wc -l < texto.txt)
+palabras=$(wc -w < texto.txt)
+caracteres=$(wc -m < texto.txt)
+
+echo "Estadisticas del archivo texto.txt:"
+echo "Numero de lineas: $lineas"
+echo "Numero de palabras: $palabras"
+echo "Numero de caracteres: $caracteres"
